c7f1c3aba503f61ac93b47081ee055553c1a0f49
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-04-07 20:05:27 +0700

    add api get contacts and get-id-by-email

--- ChatService/Core/Interfaces/IUserRepository.cs
+++ ChatService/Core/Interfaces/IUserRepository.cs
@@ -4,9 +4,10 @@ namespace ChatService.Core.Interfaces
 {
     public interface IUserRepository
     {
-        Task<User> GetUserByFirebaseUid(string firebaseUid);
         Task<User> GetUserByIdAsync(int userId);
+        Task<User> GetUserByFirebaseUid(string firebaseUid);
         Task AddConnectionIdAsync(int userId, string connectionId);
         Task RemoveConnectionIdAsync(int userId, string connectionId);
+        Task<List<User>> GetContactsAsync(int userId);
     }
 }
