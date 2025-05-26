70f721a97c005da2ff85d84fc8a6e63e73d10afe
Hugo Nguyen <21520238@gm.uit.edu.vn>
2025-05-26 17:54:59 +0700

    refactor API for event and create index for enhance time response API

--- ChatService/Controllers/RequestModels/EventRequest.cs
+++ ChatService/Controllers/RequestModels/EventRequest.cs
@@ -1,11 +1,21 @@
-﻿namespace ChatService.Controllers.RequestModels;
-
-public class EventRequest
+﻿namespace ChatService.Controllers.RequestModels
 {
-    public int CreatorId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
-    public List<int> Participants { get; set; } = new();
+    public class EventRequest
+    {
+        public int CreatorId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public List<int> Participants { get; set; } = new();
+    }
+
+    public class EventUpdateRequest
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
 }
+
