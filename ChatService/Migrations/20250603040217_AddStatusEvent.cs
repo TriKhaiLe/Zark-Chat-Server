1fc2177932fc5c983fa2495a83d58ceb23329bae
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-06-03 11:22:58 +0700

    add migration AddEventStatus

---
+++ ChatService/Migrations/20250603040217_AddStatusEvent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Events");
+        }
+    }
+}
